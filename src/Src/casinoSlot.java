6e6defc6413df44991f86fd5cdeb171fc3f385cd
Sarim <74938278+Sarim78@users.noreply.github.com>
2022-12-16 14:04:41 -0500

    Minor Edits

---
+++ src/Src/casinoSlot.java
@@ -0,0 +1,23 @@
+package Src;
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ * Casino Slot Game
+ * @athuor Sarim Siddiqui
+ */
+
+public class casinoSlot {
+
+    // Creating Objects 
+    Random rnd = new Random();
+    Scanner in = new Scanner(System.in);
+
+    // Creating Variables
+    double bet; // Player bet Variable
+
+    int maxBet = 20000; // Max bet that player can make is $20k
+    int minBet = 1000; // Min bet that player can make is $1k
+
+    double slotMachine; // Slow Machine Variable
+}
